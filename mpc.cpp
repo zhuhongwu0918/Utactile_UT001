5375c75b17347e79c96b8d931bf07d1a103dafe3
zhuhongwu0918 <zhu-hongwu@qq.com>
2024-05-19 10:41:09 +0800

    merge

--- mpc.cpp
+++ mpc.cpp
@@ -1,29 +1,7 @@
 // using hector control framework of MPC;
-<<<<<<< HEAD
+
 #include<iostream>
 using namespace std;
 void main(void){
     cout<<"hello world! \n"<<endl;
-=======
-/*=========== ***doc description @ yyp*** ===========
-This is part of Nabo (Naughty Boy, 小炮), an open project for the control of biped robot，
-Copyright (C) 2024 YYP, Shanghai Jiao Tong University, under MIT License.
-Feel free to use in any purpose, and cite Nabo or 小炮 in any style, to contribut to the advancement of the community.
-<https://github.com/tryingfly/nabo>
-<https://www.zhihu.com/column/c_1746480685987373057>
-
-=====================================================*/
-<<<<<<< HEAD
-void main(void){
-
-<<<<<<< HEAD
->>>>>>> 9972d86 (add mpc)
-=======
-#include<iostream>
-=======
->>>>>>> e8c6df9 (add mpc)
-using namespace std;
-void main(void){
-    cout<<"hello world! \n"<<endl;
->>>>>>> 5796f7f (hello test)
 }
